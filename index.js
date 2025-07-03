db047768007b4ed4c58417a6409e0e3286ca6ef2
S.H. Peduruhewa <sachinipeduruhewa@gmail.com>
2025-07-03 09:14:26 +0530

    Add initial implementation of Express server with basic routes

---
+++ index.js
@@ -0,0 +1,24 @@
+// load the require function, called that 'express'
+const express= require('express');
+const app = express();
+
+// This is a middleware function that parses incoming JSON requests
+app.get('/', (req, res) => {
+    res.send('Hello World!!!');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
+// Route Parameters
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+// This is a middleware function that parses incoming JSON requests
+// assign a port to the node application, the system attempt to read the value of a environmental variable call port, if there is a value we can use that, otherwise we can use a arbitrary number for your developer machine
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+
